0628c5b7b46a2a6ee601e5f17a922d50a65d435a
konrad <konrad.schlatte@gmail.com>
2019-01-11 21:07:49 +0000

    removed comments

--- src/apiCall.js
+++ src/apiCall.js
@@ -12,14 +12,4 @@ const apiCall = (function () {
     return { call: call };
 })();
 
-// const apiCall = function (uri) {
-//     return fetch(uri)
-//       .then( (res) => {
-//          return res
-//       })
-//       .catch( (ex) => {
-//          return 0
-//       })
-// };
-
 export default apiCall;
