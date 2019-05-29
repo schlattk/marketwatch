93d30415fcb6b0809f3a4e3413938300862ffb9c
konrad <konrad.schlatte@gmail.com>
2019-05-29 16:16:07 +0100

    add backend server

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const express = require('express');
+var cors = require('cors');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const API_PORT = 3001;
+const app = express();
+app.use(cors());
+const router = express.Router();
+var indexRouter = require('./routes/index');
+
+
+// this is our MongoDB database
+const dbRoute =
+  'mongodb://jelo:password1@ds249583.mlab.com:49583/fullstack_app';
+
+// connects our back end code with the database
+mongoose.connect(dbRoute, { useNewUrlParser: true });
+
+let db = mongoose.connection;
+
+db.once('open', () => console.log('connected to the database'));
+
+// checks if connection with the database is successful
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// (optional) only made for logging and
+// bodyParser, parses the request body to be a readable json format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use('/', indexRouter);
+
+
+// append /api for our http requests
+app.use('/api', router);
+
+// launch our backend into a port
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
